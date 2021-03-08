68db0d7741e4f4e1ee357d4b71a5acf56541d7b1
Douglas Lima Leite <douglas_leite@sicredi.com.br>
2021-03-08 13:37:21 -0300

    Commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+function App(): React.ReactElement {
+  return (
+    <>
+      <CssBaseline />
+      Teste
+    </>
+  );
+}
+
+export default App;
